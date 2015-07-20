658bc9d3b6ead7f2b96bc66a0500110452da2d66
Yossi Adi <yossiadidrum@gmail.com>
2015-07-20 18:13:54 +0300

    final changes

--- tutorials-code/multiclass/compile_and_run_multiclass.sh
+++ tutorials-code/multiclass/compile_and_run_multiclass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir bin
 javac -d bin -sourcepath . -cp ../../bin/StructED_1.0.1.jar MulticlassTutorial.java
-java -cp bin:../../bin/StructED_1.0.1.jar com.tutorials.MulticlassTutorial
+java -Xms2048m -Xmx2048m -cp bin:../../bin/StructED_1.0.1.jar com.tutorials.MulticlassTutorial
